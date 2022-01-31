bdb33aed9b2a4dd3e364e8afa2c222d1cda18acd
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2022-01-31 11:30:30 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'found-wisdom',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
