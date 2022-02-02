4da0663d36404ec4c1edf62de8e1d968564c59d7
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2022-02-02 11:10:51 -0600

    changed format of date in dummy_data to work with date-fns

--- src/dummy_data/dummy_data.js
+++ src/dummy_data/dummy_data.js
@@ -1,5 +1,20 @@
 export const DUMMY_DATA = [
-  { id: 'w1', title: 'Title 1', date: '08/20/1990', text: 'This is wisdom #1' },
-  { id: 'w2', title: 'Title 2', date: '08/22/1990', text: 'This is wisdom #2' },
-  { id: 'w3', title: 'Title 3', date: '08/23/1990', text: 'This is wisdom #3' },
+  {
+    id: 'w1',
+    title: 'Title 1',
+    date: '1990-08-20T12:00:00-00:00',
+    text: 'This is wisdom #1',
+  },
+  {
+    id: 'w2',
+    title: 'Title 2',
+    date: '1989-09-05T12:00:00-00:00',
+    text: 'This is wisdom #2',
+  },
+  {
+    id: 'w3',
+    title: 'Title 3',
+    date: '2016-02-26T12:00:00-00:00',
+    text: 'This is wisdom #3',
+  },
 ];
