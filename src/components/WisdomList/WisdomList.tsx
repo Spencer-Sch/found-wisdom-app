d77c210816203814b5973a31277394f0be8423f0
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2022-02-09 05:56:24 -0600

    added faded text placeholder in WisdomList if the user has no wisdoms added yet

--- src/components/WisdomList/WisdomList.tsx
+++ src/components/WisdomList/WisdomList.tsx
@@ -5,6 +5,14 @@ import WisdomCard from '../WisdomCard/WisdomCard';
 import './WisdomList.css';
 
 import { WisdomObj } from '../../models/WisdomObj.model';
+import {
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonItemDivider,
+  IonRow,
+  IonText,
+} from '@ionic/react';
 
 const WisdomList: React.FC = () => {
   const [storedWisdoms, setStoredWisdoms] = useState<WisdomObj[]>([]);
@@ -19,14 +27,30 @@ const WisdomList: React.FC = () => {
 
   return (
     <div>
-      {storedWisdoms.map((item: WisdomObj) => (
-        <WisdomCard
-          key={item.id}
-          id={item.id}
-          source={item.source}
-          text={item.text}
-        />
-      ))}
+      {storedWisdoms.length > 0 ? (
+        storedWisdoms.map((item: WisdomObj) => (
+          <WisdomCard
+            key={item.id}
+            id={item.id}
+            source={item.source}
+            text={item.text}
+          />
+        ))
+      ) : (
+        <IonGrid>
+          <IonRow className="ion-margin-horizontal">
+            {/* <IonItemDivider className="ion-no-padding"> */}
+            <IonCol className="ion-text-center">
+              <IonText color="dark" className="ss-larger-text">
+                <p className="ss-faded-text">
+                  You haven't added <br /> any wisdom yet...
+                </p>
+              </IonText>
+            </IonCol>
+            {/* </IonItemDivider> */}
+          </IonRow>
+        </IonGrid>
+      )}
     </div>
   );
 };
