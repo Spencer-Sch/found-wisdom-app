7cadce2c629c6c67a9d295b593c7ca350851bb14
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2022-03-02 06:37:38 -0600

    added log out functionality to the log out button in Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,9 +15,25 @@ import {
 } from '@ionic/react';
 import { home, person, cog, key } from 'ionicons/icons';
 
+import { firebase } from '../../firebase/firebase';
+import { getAuth, signOut } from 'firebase/auth';
+
 import styles from './header.module.css';
 
 const Header: React.FC = () => {
+  const logoutHandler = () => {
+    const auth = getAuth(firebase);
+    signOut(auth)
+      .then(() => {
+        console.log('logging out...');
+        // showSuccessToast('Goodbye!');
+      })
+      .catch((error) => {
+        console.log('log out: ', error);
+        // showErrorToast(error.message);
+      });
+  };
+
   return (
     <>
       <IonMenu side="start" contentId="main-content">
@@ -40,7 +56,7 @@ const Header: React.FC = () => {
               <IonIcon slot="start" icon={cog} />
               <IonLabel>Settings</IonLabel>
             </IonItem>
-            <IonItem button={true} onClick={() => console.log('Log Out')}>
+            <IonItem button={true} onClick={() => logoutHandler()}>
               <IonIcon slot="start" icon={key} />
               <IonLabel>Log Out</IonLabel>
             </IonItem>
