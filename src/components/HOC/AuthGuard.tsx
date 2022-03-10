e7f96662b8c871a3a2bb4ebe55f0d78b7a2390d1
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2022-03-10 09:43:59 -0600

    altered AuthGuard to use new AuthContext

--- src/components/HOC/AuthGuard.tsx
+++ src/components/HOC/AuthGuard.tsx
@@ -4,11 +4,22 @@ import { Redirect } from 'react-router-dom';
 import { firebase } from '../../firebase/firebase';
 import { getAuth } from 'firebase/auth';
 
+/////////////////
+import { useAuth } from '../../contexts/AuthContext';
+/////////////////
+
 const AuthGuard = (Component: any) => {
+  /////////////////
+  const { currentUser } = useAuth();
+  /////////////////
+
   class AuthHoc extends React.Component {
     authCheck = () => {
-      const user = getAuth(firebase).currentUser;
-      if (user) {
+      //////////////////
+      // const user = getAuth(firebase).currentUser;
+      // if (user) {
+      //////////////////
+      if (currentUser) {
         return <Component {...this.props} />;
       } else {
         return <Redirect to="/sign_in" />;
