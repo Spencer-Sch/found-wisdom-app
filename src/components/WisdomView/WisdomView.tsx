1f9d04574971b01f9750ac54d1d9e4caa04fcac8
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2022-04-07 10:48:19 -0500

    Removed unused imports. Removed old commented header code. Removed IonContent component. Changed Ionbutton hreft to routerLink for improved routing and page transitions.

--- src/components/WisdomView/WisdomView.tsx
+++ src/components/WisdomView/WisdomView.tsx
@@ -4,16 +4,12 @@ import { useHistory } from 'react-router-dom';
 import {
   IonButton,
   IonCol,
-  IonContent,
   IonGrid,
-  IonHeader,
   IonIcon,
   IonItemDivider,
   IonLoading,
   IonRow,
   IonText,
-  IonTitle,
-  IonToolbar,
 } from '@ionic/react';
 
 import { home } from 'ionicons/icons';
@@ -53,79 +49,77 @@ const WisdomView: React.FC<PropsData> = ({
 
   return (
     <>
-      {/* <IonHeader>
-        <IonToolbar>
-          <IonTitle className="ion-text-center">Found Wisdom</IonTitle>
-        </IonToolbar>
-      </IonHeader> */}
-      <IonContent>
-        <IonGrid
-          className={`${styles.ss_grid} ${styles.ss_move_back} ion-no-padding ion-margin-top`}
-        >
-          <IonRow>
-            <IonRow className="ion-margin-horizontal">
-              <IonItemDivider className="ion-no-padding">
-                <IonCol>
-                  <IonText color="dark" className={styles.ss_larger_text}>
-                    <p>{currentWisdom.text}</p>
-                  </IonText>
-                </IonCol>
-              </IonItemDivider>
-            </IonRow>
-            <IonRow
-              className={`${styles.ss_row} ion-margin-horizontal ion-align-items-center ion-justify-content-between`}
-            >
-              <IonCol size="auto">
-                <IonText color="medium">
-                  <p>{currentWisdom.date}</p>
+      <IonGrid
+        className={`${styles.ss_grid} ${styles.ss_move_back} ion-no-padding ion-margin-top`}
+      >
+        <IonRow>
+          <IonRow className="ion-margin-horizontal">
+            <IonItemDivider className="ion-no-padding">
+              <IonCol>
+                <IonText color="dark" className={styles.ss_larger_text}>
+                  <p>{currentWisdom.text}</p>
                 </IonText>
               </IonCol>
-              <IonCol size="auto" className="ion-justify-content-end">
-                <IonText color="medium">
-                  <p>-{currentWisdom.source}</p>
-                </IonText>
-              </IonCol>
-            </IonRow>
+            </IonItemDivider>
           </IonRow>
-          <IonRow className="ion-align-items-end ion-justify-content-end">
-            <IonCol size="12">
-              <IonButton
-                expand="full"
-                color="secondary"
-                className="ion-text-uppercase"
-                onClick={() => setShowEdit(true)}
-              >
-                edit
-              </IonButton>
-              <IonButton
-                expand="full"
-                color="danger"
-                className="ion-text-uppercase"
-                onClick={() => setShowDeleteModal(true)}
-              >
-                delete
-              </IonButton>
-              <IonButton fill="outline" expand="block" color="primary" href="/">
-                <IonIcon color="primary" slot="icon-only" icon={home} />
-              </IonButton>
+          <IonRow
+            className={`${styles.ss_row} ion-margin-horizontal ion-align-items-center ion-justify-content-between`}
+          >
+            <IonCol size="auto">
+              <IonText color="medium">
+                <p>{currentWisdom.date}</p>
+              </IonText>
+            </IonCol>
+            <IonCol size="auto" className="ion-justify-content-end">
+              <IonText color="medium">
+                <p>-{currentWisdom.source}</p>
+              </IonText>
             </IonCol>
           </IonRow>
-        </IonGrid>
-        {/* confirm delete modal */}
-        {showDeleteModal && (
-          <WisdomPageModal
-            handleDelete={handleDelete}
-            setShowDeleteModal={setShowDeleteModal}
-          />
-        )}
-        {/* Loading Spinner */}
-        <IonLoading
-          isOpen={loading}
-          spinner="lines-sharp"
-          cssClass={styles.my_custom_spinner}
-          message="WisdomView deleting wisdom..."
+        </IonRow>
+        <IonRow className="ion-align-items-end ion-justify-content-end">
+          <IonCol size="12">
+            <IonButton
+              expand="full"
+              color="secondary"
+              className="ion-text-uppercase"
+              onClick={() => setShowEdit(true)}
+            >
+              edit
+            </IonButton>
+            <IonButton
+              expand="full"
+              color="danger"
+              className="ion-text-uppercase"
+              onClick={() => setShowDeleteModal(true)}
+            >
+              delete
+            </IonButton>
+            <IonButton
+              fill="outline"
+              expand="block"
+              color="primary"
+              routerLink="/"
+            >
+              <IonIcon color="primary" slot="icon-only" icon={home} />
+            </IonButton>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+      {/* confirm delete modal */}
+      {showDeleteModal && (
+        <WisdomPageModal
+          handleDelete={handleDelete}
+          setShowDeleteModal={setShowDeleteModal}
         />
-      </IonContent>
+      )}
+      {/* Loading Spinner */}
+      <IonLoading
+        isOpen={loading}
+        spinner="lines-sharp"
+        cssClass={styles.my_custom_spinner}
+        message="WisdomView deleting wisdom..."
+      />
     </>
   );
 };
