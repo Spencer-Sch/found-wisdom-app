78b432fd4c18aa3335227873fbb5aacf40538719
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2022-04-13 13:15:28 -0500

    removed unused imports. removed 'exact' attribute from routes where not needed. removed old commented route and header code. changed previous route '/wisdom/add' to '/add' to eliminate potential mismatch problems with 'wisdom/:wisdomid' route.

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,5 @@
 import { Redirect, Route } from 'react-router-dom';
-import {
-  IonApp,
-  IonContent,
-  IonRouterOutlet,
-  setupIonicReact,
-} from '@ionic/react';
+import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 
 /* Core CSS required for Ionic components to work properly */
@@ -27,12 +22,10 @@ import '@ionic/react/css/display.css';
 import './theme/variables.css';
 
 import Header from './components/Header/Header';
-import Home from './pages/Home/Home';
 import HomeWrapper from './pages/Home/HomeWrapper';
 import AuthGuard from './components/HOC/AuthGuard';
 import SignInOrRegister from './pages/SignInOrRegister';
 import WisdomPageWrapper from './pages/WisdomPage/WisdomPageWrapper';
-// import WisdomPage from './pages/WisdomPage/WisdomPage';
 import WisdomAdd from './pages/WisdomAdd/WisdomAdd';
 
 import { useAuth } from './contexts/AuthContext';
@@ -48,16 +41,14 @@ const Routes: React.FC = () => {
         {currentUser ? <Header /> : null}
         <IonRouterOutlet id="ion-router-outlet">
           <Route
-            exact
             path="/wisdom/:wisdomid"
             component={AuthGuard(WisdomPageWrapper)}
           />
-          <Route exact path="/wisdom/add" component={AuthGuard(WisdomAdd)} />
-          <Route exact path="/home">
+          <Route path="/add" component={AuthGuard(WisdomAdd)} />
+          <Route path="/home">
             <Redirect to="/" />
           </Route>
           <Route
-            exact
             path="/sign_in"
             component={(props: any) => <SignInOrRegister {...props} />}
           />
@@ -66,39 +57,6 @@ const Routes: React.FC = () => {
       </IonReactRouter>
     </IonApp>
   );
-  // return (
-  //   <IonApp>
-  //     {/* ///////////////////////////////// */}
-  //     {/* {currentUser ? <Header /> : null} */}
-  //     <Header />
-  //     <IonContent id="main-content">
-  //     {/* ///////////////////////////////// */}
-  //       <IonReactRouter>
-  //     {/* ///////////////////////////////// */}
-  //        <Header />
-  //        <IonRouterOutlet id="main-content">
-  //        {/* <IonRouterOutlet> */}
-  //     {/* ///////////////////////////////// */}
-  //           <Route
-  //             exact
-  //             path="/wisdom/:wisdomid"
-  //             component={AuthGuard(WisdomPageWrapper)}
-  //           />
-  //           <Route exact path="/wisdom/add" component={AuthGuard(WisdomAdd)} />
-  //           <Route exact path="/home">
-  //             <Redirect to="/" />
-  //           </Route>
-  //           <Route
-  //             exact
-  //             path="/sign_in"
-  //             component={(props: any) => <SignInOrRegister {...props} />}
-  //           />
-  //           <Route exact path="/" component={AuthGuard(HomeWrapper)} />
-  //         </IonRouterOutlet>
-  //       </IonReactRouter>
-  //     </IonContent>
-  //   </IonApp>
-  // );
 };
 
 export default Routes;
