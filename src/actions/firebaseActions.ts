6e5a093452aa481cd9446689f5a6985a42019932
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2022-06-14 12:31:28 -0500

    removed old commented code in preparation for merge into master branch

--- src/actions/firebaseActions.ts
+++ src/actions/firebaseActions.ts
@@ -1,15 +1,10 @@
-import {
-  firestoreDB,
-  wisdomsCollectionDocId,
-  usersCollectionDocId,
-} from '../firebase/firebase';
+import { firestoreDB } from '../firebase/firebase';
 import {
   doc,
   getDocs,
   updateDoc,
   collection,
   query,
-  deleteField,
   setDoc,
   DocumentReference,
   DocumentData,
@@ -20,18 +15,15 @@ import {
 } from 'firebase/firestore';
 
 import {
-  createNewUserObj,
   createNewUserPrivObj,
   createNewUserPubObj,
 } from '../functions/userFunctions';
 import {
   WisdomData,
   WisdomObj,
-  UsersCollectionUserObj,
   AddNewWisdomToFirestore,
 } from '../models/models';
 import {
-  buildNewWisdom,
   filterDeletedItem,
   getNextWisdomId,
 } from '../functions/wisdomFunctions';
@@ -39,7 +31,6 @@ import {
 /////////////////////////////////////
 // CONSTANTS
 /////////////////////////////////////
-// MOVE TO FIREBASE.TS ???
 
 const USERS_COLLECTION = 'usersCollection';
 const WISDOMS_COLLECTION = 'wisdomsCollection';
@@ -48,7 +39,6 @@ export const wisdomsCollection = collection(firestoreDB, WISDOMS_COLLECTION);
 export const usersCollection = collection(firestoreDB, USERS_COLLECTION);
 
 const Q_WISDOMS_COLLECTION = query(wisdomsCollection);
-const Q_USERS_COLLECTION = query(usersCollection);
 
 /////////////////////////////////////
 // TYPES
@@ -80,28 +70,13 @@ type UploadNewWisdom = (newWisdom: WisdomObj) => void;
 
 type AddToUserWisdomIdList = (uid: string, wisdomId: string) => void;
 
-// type AddToUserWisdomCollections = (
-//   username: string,
-//   wisdomId: string,
-//   userWisdomCollections: {
-//     default: string[];
-//     nextWisdomToPush: string | null;
-//     userCreatedCategory?: string[] | undefined;
-//   }
-// ) => void;
+type UpdateUsersNextWisdomToPush = (uid: string, wisdomId: string) => void;
 
 type RemoveFromUserWisdomIds = (
   username: string,
   filteredCollection: string[] | [],
   newNextWisdomToPush: string | null
 ) => void;
-
-// type RemoveFromUserWisdomCollections = (
-//   username: string,
-//   filteredCollection: string[] | [],
-//   userNextWisdomToPush: string | null,
-//   newNextWisdomToPush: string | null
-// ) => void;
 
 type RemoveWisdomFromWisdomsCollection = (wisdomId: string) => void;
 
@@ -172,21 +147,6 @@ export const addUserToDB: AddUserToDB = async (
   }
 };
 
-///////////////////////////////////
-// Previous addUserToDB function
-///////////////////////////////////
-// export const addUserToDB: AddUserToDB = async (username, email, password) => {
-//   const newUserObj = createNewUserObj(username, email, password);
-
-//   const docRef = doc(usersCollection, usersCollectionDocId);
-//   await updateDoc(docRef, { [`${username}`]: { ...newUserObj } });
-//   // try {
-//   // } catch (error) {
-//   //   console.error('Error from create addUserToDB: ', error);
-//   // }
-// };
-///////////////////////////////////
-
 export const fetchUserPrivData: FetchUserPrivData = async (uid) => {
   try {
     const docRef = doc(usersCollection, uid, 'user_priv', 'user_priv');
@@ -206,20 +166,6 @@ export const fetchUserPrivData: FetchUserPrivData = async (uid) => {
     console.error('firebaseActions -> fetchUserPrivData Error:', error);
   }
 };
-
-// export const fetchUserData: FetchUserData = (username) => {
-//   return getDocs(Q_USERS_COLLECTION)
-//     .then((snapshot) => {
-//       const userDocs = snapshot.docs.map((item) => ({
-//         ...item.data(),
-//       }));
-//       return userDocs[0][`${username}`];
-//     })
-//     .catch((error) => {
-//       // improve error handeling!!!
-//       console.error('fetchUserData Error:', error);
-//     });
-// };
 
 export const fetchWisdomsById: FetchWisdomsById = (wisdomIds) => {
   if (wisdomIds.length === 0) {
@@ -246,28 +192,6 @@ export const fetchWisdomsById: FetchWisdomsById = (wisdomIds) => {
       return null;
     });
 };
-// export const fetchWisdomsById: FetchWisdomsById = (wisdomIds) => {
-//   if (wisdomIds.length === 0) {
-//     return [];
-//   }
-//   return getDocs(Q_WISDOMS_COLLECTION)
-//     .then((snapshot) => {
-//       const wisdomsDoc = snapshot.docs.map((item) => ({
-//         ...item.data(),
-//       }));
-
-//       const userWisdoms: WisdomData[] | [] = wisdomIds.map((item) => {
-//         return wisdomsDoc[0][item]['wisdomData'];
-//       });
-
-//       return userWisdoms.length > 0 ? userWisdoms : null;
-//     })
-//     .catch((error) => {
-//       // improve error handeling!!!
-//       console.error(error);
-//       return null;
-//     });
-// };
 
 export const fetchCurrentWisdom: FetchCurrentWisdom = (wisdomid) => {
   return getDocs(Q_WISDOMS_COLLECTION)
@@ -290,21 +214,10 @@ export const uploadEditedWisdom: UploadEditedWisdom = async (editedWisdom) => {
   await updateDoc(docRef, { wisdomData: { ...editedWisdom } });
 };
 
-// export const uploadEditedWisdom: UploadEditedWisdom = async (editedWisdom) => {
-//   const wisdomPath = `${editedWisdom.id}.wisdomData`;
-//   const docRef = doc(wisdomsCollection, wisdomsCollectionDocId);
-//   await updateDoc(docRef, { [`${wisdomPath}`]: { ...editedWisdom } });
-// };
-
 export const uploadNewWisdom: UploadNewWisdom = async (newWisdom) => {
   const docRef = doc(wisdomsCollection, newWisdom.wisdomData.id);
   await setDoc(docRef, { ...newWisdom });
 };
-
-// export const uploadNewWisdom: UploadNewWisdom = async (newWisdom) => {
-//   const docRef = doc(wisdomsCollection, wisdomsCollectionDocId);
-//   await updateDoc(docRef, { [newWisdom.wisdomData.id]: { ...newWisdom } });
-// };
 
 const addToUserWisdomIdList: AddToUserWisdomIdList = async (uid, wisdomId) => {
   const next_wisdom_to_push_DocRef = doc(
@@ -374,33 +287,6 @@ const addToUserWisdomIdList: AddToUserWisdomIdList = async (uid, wisdomId) => {
   }
 };
 
-// export const addToUserWisdomCollections: AddToUserWisdomCollections = async (
-//   username,
-//   wisdomId,
-//   userWisdomCollections
-// ) => {
-//   const docRef = doc(usersCollection, usersCollectionDocId);
-//   const userWisdomCollectionsPath = `${username}.wisdomCollections`;
-//   const nextWisdomToPush = userWisdomCollections.nextWisdomToPush;
-//   const defaultCollection = userWisdomCollections.default;
-
-//   if (nextWisdomToPush === null) {
-//     await updateDoc(docRef, {
-//       [userWisdomCollectionsPath]: {
-//         default: [...defaultCollection, wisdomId],
-//         nextWisdomToPush: wisdomId,
-//       },
-//     });
-//   } else {
-//     await updateDoc(docRef, {
-//       [userWisdomCollectionsPath]: {
-//         default: [...defaultCollection, wisdomId],
-//         nextWisdomToPush: nextWisdomToPush,
-//       },
-//     });
-//   }
-// };
-
 export const addNewWisdomToFirestore: AddNewWisdomToFirestore = async (
   newWisdom,
   uid
@@ -412,21 +298,6 @@ export const addNewWisdomToFirestore: AddNewWisdomToFirestore = async (
   const wisdomId = newWisdom.wisdomData.id;
   addToUserWisdomIdList(uid, wisdomId);
 };
-
-// export const addNewWisdomToFirestore: AddNewWisdomToFirestore = async (
-//   values,
-//   username
-// ) => {
-//   // create and upload new wisdom to wisdomsCollection
-//   const newWisdom = buildNewWisdom(values, username);
-//   uploadNewWisdom(newWisdom);
-
-//   // upload new wisdom to user's wisdomCollections
-//   const userData = await fetchUserData(username);
-//   const userWisdomCollections = userData.wisdomCollections;
-//   const wisdomId = newWisdom.wisdomData.id;
-//   addToUserWisdomCollections(username, wisdomId, userWisdomCollections);
-// };
 
 export const removeFromUserWisdomIds: RemoveFromUserWisdomIds = async (
   uid,
@@ -471,33 +342,6 @@ export const removeFromUserWisdomIds: RemoveFromUserWisdomIds = async (
   }
 };
 
-// export const removeFromUserWisdomCollections: RemoveFromUserWisdomCollections =
-//   async (
-//     uid,
-//     filteredCollection,
-//     userNextWisdomToPush,
-//     newNextWisdomToPush
-//   ) => {
-//     const docRef = doc(usersCollection, usersCollectionDocId);
-//     const userWisdomCollectionsPath = `${uid}.wisdomCollections`;
-
-//     if (newNextWisdomToPush === '') {
-//       await updateDoc(docRef, {
-//         [userWisdomCollectionsPath]: {
-//           default: [...filteredCollection],
-//           nextWisdomToPush: userNextWisdomToPush,
-//         },
-//       });
-//     } else {
-//       await updateDoc(docRef, {
-//         [userWisdomCollectionsPath]: {
-//           default: [...filteredCollection],
-//           nextWisdomToPush: newNextWisdomToPush,
-//         },
-//       });
-//     }
-//   };
-
 export const removeWisdomFromWisdomsCollection: RemoveWisdomFromWisdomsCollection =
   async (wisdomId) => {
     try {
@@ -523,10 +367,6 @@ export const deleteWisdomFromFirestore: DeleteWisdomFromFirestore = async (
     'next_wisdom_to_push'
   );
 
-  /////////////////////
-  // const wisdoms_all_snapshot = await getDoc(wisdoms_all_docRef);
-  // const next_wisdom_to_push_snapshot = await getDoc(next_wisdom_to_push_docRef);
-
   let wisdoms_all_snapshot;
   let next_wisdom_to_push_snapshot;
 
@@ -538,7 +378,6 @@ export const deleteWisdomFromFirestore: DeleteWisdomFromFirestore = async (
     console.error('firebaseActions.ts -> deleteWisdomFromFirestore: ', e);
     return;
   }
-  /////////////////////
 
   const wisdoms_all_data = wisdoms_all_snapshot.data();
   const next_wisdom_to_push_data = next_wisdom_to_push_snapshot.data();
@@ -565,83 +404,3 @@ export const deleteWisdomFromFirestore: DeleteWisdomFromFirestore = async (
   removeFromUserWisdomIds(uid, filteredCollection, newNextWisdomToPush);
   removeWisdomFromWisdomsCollection(wisdomId);
 };
-
-// export const deleteWisdomFromFirestore: DeleteWisdomFromFirestore = async (
-//   username,
-//   wisdomId
-// ) => {
-//   const { wisdomCollections } = await fetchUserData(username);
-//   const userNextWisdomToPush = wisdomCollections.nextWisdomToPush;
-//   const userWisdoms = wisdomCollections.default;
-//   let newNextWisdomToPush: string | null = '';
-
-//   if (userNextWisdomToPush === wisdomId && userWisdoms.length > 1) {
-//     newNextWisdomToPush = getNextWisdomId(wisdomId, userWisdoms);
-//   }
-
-//   if (userNextWisdomToPush === wisdomId && userWisdoms.length === 1) {
-//     newNextWisdomToPush = null;
-//   }
-
-//   const filteredCollection = filterDeletedItem(userWisdoms, wisdomId);
-
-//   removeFromUserWisdomCollections(
-//     username,
-//     filteredCollection,
-//     userNextWisdomToPush,
-//     newNextWisdomToPush
-//   );
-
-//   removeWisdomFromWisdomsCollection(wisdomId);
-// };
-
-////////////////////////////////////////
-// OLD PUSH NOTIFICATION CODE BACKUP
-////////////////////////////////////////
-
-// const pushNotification = () => {
-//   //////////////////////////////////////////
-//   // REFACTOR???
-//   //////////////////////////////////////////
-//   if (!storedWisdoms) {
-//     // improve error handeling!!!
-//     console.error('from pushNotification in Home: storedWisdoms is null!');
-//     return;
-//   }
-//   //////////////////////////////////////////
-//   let wisdomToShow: WisdomData;
-//   let nextWisdom: WisdomData;
-//   const editedState = [...storedWisdoms];
-
-//   storedWisdoms.forEach((item, idx) => {
-//     if (item.next === true) {
-//       wisdomToShow = {
-//         ...item,
-//         next: false,
-//       };
-
-//       editedState[idx] = { ...wisdomToShow };
-
-//       if (idx === storedWisdoms.length - 1) {
-//         nextWisdom = {
-//           ...storedWisdoms[0],
-//           next: true,
-//         };
-
-//         editedState[0] = { ...nextWisdom };
-//       } else {
-//         nextWisdom = {
-//           ...storedWisdoms[idx + 1],
-//           next: true,
-//         };
-
-//         editedState[idx + 1] = { ...nextWisdom };
-//       }
-//     }
-//   });
-
-//   localStorage.setItem('myWisdoms', JSON.stringify(editedState));
-//   setStoredWisdoms(editedState);
-
-//   alert(wisdomToShow!.text);
-// };
