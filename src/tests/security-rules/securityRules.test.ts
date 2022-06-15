d75c874ac3c208017465820f317d1efb36e58e22
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2022-06-15 06:26:08 -0500

    Setup project for testing Firebase security rules with jest and typescript.
    I added the firebase SDK, as well as the firebase emulators, to my project to facilitate writing and testing security rules in VS code. I had to add a couple packages to get jest and typescript to work together. I have set up an initial securityRules.test.ts file that is functionaing appropriately.

---
+++ src/tests/security-rules/securityRules.test.ts
@@ -0,0 +1,83 @@
+import { firestore } from '@firebase/testing';
+const firebase = require('../../../node_modules/@firebase/testing');
+
+/*====================
+ CLI COMMANDS
+====================*/
+
+// to run emulators:
+// $ firebase emulators:start
+
+// to start up the rules test for jest:
+// $ npm run test:rules
+
+/*====================
+ INTERFACES
+====================*/
+
+interface MyAuth {
+  uid: string;
+  email: string;
+}
+
+/*====================
+ TYPES
+====================*/
+
+type GetFirestore = (auth: MyAuth | null) => firestore.Firestore;
+type GetAdminFirestore = () => firestore.Firestore;
+
+/*====================
+ CONSTANTS
+====================*/
+
+const MY_PROJECT_ID = 'foundwisdom-76365';
+// const myId = 'user_abc';
+// const theirId = 'user_xyz';
+// const myAuth = { uid: myId, email: 'abc@gmail.com' };
+const myId: string = 'user_abc';
+const theirId: string = 'user_xyz';
+const myAuth: MyAuth = { uid: myId, email: 'abc@gmail.com' };
+
+/*====================
+ HELPER FUNCTIONS
+====================*/
+
+const getFirestore: GetFirestore = (auth) => {
+  return firebase
+    .initializeTestApp({ projectId: MY_PROJECT_ID, auth: auth })
+    .firestore();
+};
+
+const getAdminFirestore: GetAdminFirestore = () => {
+  return firebase.initializeAdminApp({ projectId: MY_PROJECT_ID }).firestore();
+};
+
+/*====================
+ TEST SETUP
+====================*/
+
+beforeEach(async () => {
+  await firebase.clearFirestoreData({ projectId: MY_PROJECT_ID });
+});
+
+/*====================
+ TESTS
+====================*/
+
+describe('Security Rules Tests', () => {
+  test('basic read test against rules', async () => {
+    // const db = getFirestore(null);
+    const db = getFirestore(null);
+    const testDoc = db.collection('usersCollection').doc('testDoc');
+    return firebase.assertSucceeds(testDoc.get()).then(() => {});
+  });
+});
+
+/*====================
+ TEST TEARDOWN
+====================*/
+
+afterAll(async () => {
+  await firebase.clearFirestoreData({ projectId: MY_PROJECT_ID });
+});
