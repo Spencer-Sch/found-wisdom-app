c35f9096ebbd57307359516738f20d1026bcb232
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2022-06-16 12:22:27 -0500

    Wrote tests for 'allow create' security rules for usersCollection/{userId}/wisdom_ids/user_categories.

---
+++ src/tests/security-rules/wisdom_ids/user_categories-rules-tests/userCategoriesCreate.test.ts
@@ -0,0 +1,123 @@
+import { firestore } from '@firebase/testing';
+const firebase = require('../../../../../node_modules/@firebase/testing');
+
+/*====================
+ CLI COMMANDS
+====================*/
+
+// to run emulators:
+// $ firebase emulators:start
+
+// to start up the rules test for jest:
+// $ npm run test:rules
+
+/*====================
+ CONSTANTS
+====================*/
+
+const MY_PROJECT_ID = 'foundwisdom-76365';
+const myUid: string = 'user_abc';
+const theirUid: string = 'user_xyz';
+const myAuth: MyAuth = { uid: myUid, email: 'abc@gmail.com' };
+const USERS_COLLECTION = 'usersCollection';
+const WISDOM_IDS = 'wisdom_ids';
+const USER_CATEGORIES = 'user_categories';
+
+const user_categories_correct_doc = {
+  userCategories: [],
+};
+
+/*====================
+ INTERFACES
+====================*/
+
+interface MyAuth {
+  uid: string;
+  email: string;
+}
+
+/*====================
+ TYPES
+====================*/
+
+type GetFirestore = (auth: MyAuth | null) => firestore.Firestore;
+type GetTestDoc = (
+  auth: MyAuth | null,
+  docId: string
+) => firestore.DocumentReference<firestore.DocumentData>;
+
+/*====================
+ HELPER FUNCTIONS
+====================*/
+
+const getFirestore: GetFirestore = (auth) => {
+  return firebase
+    .initializeTestApp({ projectId: MY_PROJECT_ID, auth: auth })
+    .firestore();
+};
+
+const getTestDoc: GetTestDoc = (auth, docId) => {
+  const db = getFirestore(auth);
+  return db
+    .collection(USERS_COLLECTION)
+    .doc(docId)
+    .collection(WISDOM_IDS)
+    .doc(USER_CATEGORIES);
+};
+
+/*====================
+ TEST SETUP
+====================*/
+
+beforeEach(async () => {
+  await firebase.clearFirestoreData({ projectId: MY_PROJECT_ID });
+});
+
+/*====================
+ TESTS
+====================*/
+
+describe('Tests for usersCollection/{userId}/wisdom_ids/user_categories "allow create" security rules', () => {
+  test("unathorized user can't create usersCollection/{userId}/wisdom_ids/user_categories doc", async () => {
+    const testCreate = getTestDoc(null, myUid);
+    await firebase.assertFails(testCreate.set(user_categories_correct_doc));
+  });
+  test("authorized user can't create a usersCollection/{userId}/wisdom_ids/user_categories doc under a different userId", async () => {
+    const testCreate = getTestDoc(myAuth, theirUid);
+    await firebase.assertFails(testCreate.set(user_categories_correct_doc));
+  });
+  test('authorized user can create their own usersCollection/{userId}/wisdom_ids/user_categories doc as long as the doc meets all criteria', async () => {
+    const testCreate = getTestDoc(myAuth, myUid);
+    await firebase.assertSucceeds(testCreate.set(user_categories_correct_doc));
+  });
+  test("authorized user can't create their own usersCollection/{userId}/wisdom_ids/user_categories doc with unrecognized field(s)", async () => {
+    const testDoc = {
+      userCategories: [],
+      favorite_color: 'green', // unrecognized field
+    };
+    const testCreate = getTestDoc(myAuth, myUid);
+    await firebase.assertFails(testCreate.set(testDoc));
+  });
+  test("authorized user can't create usersCollection/{userId}/wisdom_ids/user_categories doc if 'userCategories' field is missing", async () => {
+    const testDoc = {
+      // userCategories: [], is missing
+    };
+    const testCreate = getTestDoc(myAuth, myUid);
+    await firebase.assertFails(testCreate.set(testDoc));
+  });
+  test("authorized user can't create usersCollection/{userId}/wisdom_ids/user_categories doc if 'userCategories' field is not an array", async () => {
+    const testDoc = {
+      userCategories: 123,
+    };
+    const testCreate = getTestDoc(myAuth, myUid);
+    await firebase.assertFails(testCreate.set(testDoc));
+  });
+});
+
+/*====================
+ TEST TEARDOWN
+====================*/
+
+afterAll(async () => {
+  await firebase.clearFirestoreData({ projectId: MY_PROJECT_ID });
+});
