55830a2d0eb62596f244a1e9d4082e87996869f2
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2022-07-14 06:59:37 -0500

    added a useEffect call in LogInOrRegister.tsx to clear userWisdoms context after a user logs out.

--- src/pages/LogInOrRegister/LogInOrRegister.tsx
+++ src/pages/LogInOrRegister/LogInOrRegister.tsx
@@ -1,16 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { IonContent, IonPage } from '@ionic/react';
 
 import LogInForm from '../../components/LogInForm/LogInForm';
 import RegisterForm from '../../components/RegisterForm/RegisterForm';
 import { useAuth } from '../../contexts/AuthContext';
+import { useWisdomStore } from '../../contexts/WisdomStoreContext';
 
 import styles from './logInOrRegister.module.css';
 
 const LogInOrRegister: React.FC = () => {
   const [showRegisterForm, setShowRegisterForm] = useState(false);
   const { currentUser } = useAuth();
+  const { setUserWisdoms } = useWisdomStore();
+
+  useEffect(() => {
+    setUserWisdoms!(null);
+  }, [setUserWisdoms]);
 
   return (
     <>
